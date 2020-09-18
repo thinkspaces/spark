9ffdde04f747e95108109cfa645c84c8a4aa8c80
Brandon Palomino <12bpalomino@gmail.com>
2020-09-18 01:30:39 -0700

    feat(design-tokens): adding color tokens docs

---
+++ packages/@thinkspaces/tokens/docs/components/index.jsx
@@ -0,0 +1,51 @@
+/* eslint import/prefer-default-export: 0, react/prop-types: 0 */
+import React from 'react';
+
+export const ColorBlock = ({ labelColor, label, color }) => (
+  <div
+    style={{
+      display: 'flex',
+      flex: 1,
+      justifyContent: 'space-between',
+      padding: 12,
+      background: color,
+      color: labelColor,
+    }}
+  >
+    <span>{label}</span>
+    <span>{color}</span>
+  </div>
+);
+
+export const ColorBlockList = ({
+  label = '',
+  labelColor = 'black',
+  colors,
+}) => {
+  return (
+    <div>
+      {Object.keys(colors).map((color, idx) => {
+        if (idx > 4) labelColor = 'white';
+        return (
+          <ColorBlock
+            labelColor={labelColor}
+            label={`${label} ${color}`}
+            color={colors[color]}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export const GridContainer = ({ children }) => (
+  <div
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(220px, 1fr))',
+      gridGap: 20,
+    }}
+  >
+    {children}
+  </div>
+);
