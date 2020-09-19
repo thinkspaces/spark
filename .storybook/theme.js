d1e1c3213ab4b63688112b84b4310679ea49bb18
Brandon Palomino <12bpalomino@gmail.com>
2020-09-19 14:48:07 -0700

    docs: defaulting to docs tab hiding the rest

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  appBg: 'rgb(250,250,250)',
+
+  brandTitle: 'Spark',
+  brandImage: 'https://thinkspaces.org/static/media/logo-circle.69eaca25.png',
+});
